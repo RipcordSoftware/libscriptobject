be438bd26e2a41bd925124da72075f661127cf70
craigminihan <craig@ripcordsoftware.com>
2015-04-09 16:37:03 +0100

    First time add of testlibscriptobject project

---
+++ src/testlibscriptobject/main.cpp
@@ -0,0 +1,39 @@
+/*
+ *  This file is part of libscriptobject.
+ *
+ *  libscriptobject is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libscriptobject is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libscriptobject.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include <iostream>
+#include <fstream>
+
+#include <json/value.h>
+#include <json/reader.h>
+
+#include "script_object_source.h"
+
+using namespace std;
+
+int main() {
+    
+    std::ifstream istream;
+    istream.open("test1.json", std::ios::in | std::ios::binary);
+    
+    Json::Value root;
+    Json::Reader reader;
+    reader.parse(istream, root);
+
+    return 0;
+}
+
